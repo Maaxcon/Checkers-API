6f52965d57b55aa75b0866991788217a740ebe2c
Maaxcon <127834559+Maaxcon@users.noreply.github.com>
2026-04-14 13:58:50 +0300

    feat: add CORS middleware and allow all origins for development

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS configuration (development)
+CORS_ALLOW_ALL_ORIGINS = True
