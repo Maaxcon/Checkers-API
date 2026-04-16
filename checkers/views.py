574bff2d4b2c258340c17a88a857d9f91decc271
Maaxcon <127834559+Maaxcon@users.noreply.github.com>
2026-04-16 10:31:28 +0300

    feat: add get_game API endpoint to retrieve game details by ID

--- checkers/views.py
+++ checkers/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import Game
 from engine.board import create_initial_board
@@ -19,3 +20,17 @@ def create_game(request):
         'winner': game.winner,
         'time_remaining': game.player_time_remaining,
     }, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def get_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+
+    return Response({
+        'id': str(game.id),
+        'status': game.status,
+        'board': game.board,
+        'turn': game.current_turn,
+        'winner': game.winner,
+        'time_remaining': game.player_time_remaining,
+    }, status=status.HTTP_200_OK)
